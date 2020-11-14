bb1e91ce431f7865675f0740872ad0709ae2545f
Luan Ogawa <ogawa.luan@gmail.com>
2020-11-14 04:22:36 -0300

    creating pages, components and error handling

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn/index';
+import SignUp from './pages/SignUp/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;

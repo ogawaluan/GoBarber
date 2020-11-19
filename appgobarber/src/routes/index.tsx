2585129de2f9896dbd67ea1999d74154a8b6214b
Luan Ogawa <ogawa.luan@gmail.com>
2020-11-18 22:46:15 -0300

    creating pages and handling errors

--- appgobarber/src/routes/index.tsx
+++ appgobarber/src/routes/index.tsx
@@ -12,7 +12,7 @@ const AuthRoutes: React.FC = () => {
       screenOptions={{
         headerShown: false,
         cardStyle: { backgroundColor: '#312e38' },
-      }} 
+      }}
     >
       <Auth.Screen name="SignIn" component={SignIn} />
       <Auth.Screen name="SignUp" component={SignUp} />

050683b59077a442de8a583abcb491a822edbfcd
Luan Ogawa <luanogawa@localhost.localdomain>
2021-01-22 19:21:39 -0300

    adding redis

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -8,7 +8,7 @@ import uploadConfig from '@config/upload';
 import EnsureAuthenticated from '../middlewares/EnsureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 

5a4ec87ad600b6a70c1906f61f0455bf2bc61e5f
ogawaluan <ogawa.luan@gmail.com>
2021-02-05 20:19:48 -0300

    finished Dashboard and CreateAppointment pages

--- appgobarber/src/hooks/auth.tsx
+++ appgobarber/src/hooks/auth.tsx
@@ -40,6 +40,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -60,7 +62,9 @@ const AuthProvider: React.FC = ({ children }) => {
     await AsyncStorage.multiSet([
       ['@GoBarber:user', JSON.stringify(user)],
       ['@GoBarber:token', token]
-    ])
+    ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
@@ -71,7 +75,7 @@ const AuthProvider: React.FC = ({ children }) => {
       '@GoBarber:token',
     ]);
 
-    setData({} as AuthState)
+    setData({} as AuthState);
   }, []);
 
   return (
